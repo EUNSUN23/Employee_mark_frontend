7592275460ba6e7e6433bd7afe576561b67be087
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2021-03-30 13:14:47 +0900

    STAT:DeptChart.js) disableNav추가

--- src/components/Statistics/Graph/SalaryStackChart/DeptChart.js
+++ src/components/Statistics/Graph/SalaryStackChart/DeptChart.js
@@ -88,11 +88,17 @@ const useStyles = makeStyles(() => ({
     fontSize: 80,
     color: "#333",
   },
+  disabled: {
+    fontSize: 80,
+    color: "#333",
+    color: "grey",
+  },
 }));
 
 const DeptChart = ({ deptData }) => {
   const classes = useStyles();
   const [value, setValue] = useState(40000);
+  const [disabledNav, setDisabledNav] = useState("backward");
 
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
@@ -110,13 +116,13 @@ const DeptChart = ({ deptData }) => {
     "Sales",
   ];
 
-  let mySwiper;
+  let chartSwiper;
 
   useEffect(() => {
     if (swiperRef.current) {
-      mySwiper = swiperRef.current.swiper;
+      chartSwiper = swiperRef.current.swiper;
     }
-  }, [value]);
+  });
 
   const onClickFilter = useCallback(
     (icon) => {
@@ -126,116 +132,132 @@ const DeptChart = ({ deptData }) => {
     [value]
   );
 
-  const makeDeptChart = () => {
-    if (!deptData) return null;
-
-    return (
-      <>
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-          className={classes.titleContainer}
-        >
-          <Grid item>
-            <h1 ref={paginationRef} className="swiperPagination"></h1>
-          </Grid>
-          <SalaryFilter
-            onClickFilter={onClickFilter}
-            value={value}
-            className={classes.title}
-          />
-        </Grid>
-        <Swiper
-          ref={swiperRef}
-          slidesPerView={1}
-          id="main"
-          spaceBetween={40}
-          pagination={{
-            el: ".swiperPagination",
-            type: "custom",
-            renderCustom: (swiper, current, total) => {
-              console.log("index", current);
-              const deptName = deptList[current - 1];
-              return deptName;
-            },
-          }}
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-            swiper.params.pagination.el = paginationRef.current;
-          }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => {
-            console.log(swiper);
-          }}
-          onSlideChange={(swiper) => console.log("slide change", swiper)}
-          className={classes.root}
-        >
-          {Object.keys(deptData).map((dept, idx) => {
-            const data = deptData[dept];
-            return (
-              <SwiperSlide
-                key={`chart-${dept}`}
-                className={classes.swiperSlide}
-              >
-                <ResponsiveContainer
-                  width="100%"
-                  height={380}
-                  className={classes.chartContainer}
-                >
-                  <BarChart data={data} margin={{ top: 50 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="sal" tick={false} />
-                    <YAxis dataKey="cnt" />
-
-                    <Bar
-                      dataKey="cnt"
-                      fill="#8884d8"
-                      label={<CustomizedLabel currentVal={value} />}
-                    >
-                      {data.map((entry, index) => {
-                        const highlight = entry.sal === value;
-                        const color = setChartColor(entry.dept_name, highlight);
-                        return (
-                          <Cell
-                            fill={color}
-                            key={`cell-${index}`}
-                            stroke="false"
-                            strokeDasharray="5,5"
-                          />
-                        );
-                      })}
-                    </Bar>
-                  </BarChart>
-                </ResponsiveContainer>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        <div
-          ref={navigationPrevRef}
-          onClick={() => mySwiper.slidePrev()}
-          className={classes.backward}
-        >
-          <ArrowLeftIcon className={classes.moveInner} />
-        </div>
-        <div
-          ref={navigationNextRef}
-          onClick={() => mySwiper.slideNext()}
-          className={classes.forward}
-        >
-          <ArrowRightIcon className={classes.moveInner} />
-        </div>
-      </>
-    );
+  const onClickForward = (chartSwiper) => {
+    if (chartSwiper.isEnd) return;
+    if (disabledNav === "backward") setDisabledNav(null);
+    chartSwiper.slideNext();
   };
 
-  return makeDeptChart();
+  const onClickBackward = (chartSwiper) => {
+    if (chartSwiper.isBeginning) return;
+    if (disabledNav === "forward") setDisabledNav(null);
+    chartSwiper.slidePrev();
+  };
+
+  const disableNav = (chartSwiper) => {
+    if (!chartSwiper.isEnd && !chartSwiper.isBeginning) return;
+    chartSwiper.isEnd && setDisabledNav("forward");
+    chartSwiper.isBeginning && setDisabledNav("backward");
+  };
+  const forwardClass = disabledNav === "forward" ? "disabled" : "moveInner";
+  const backwardClass = disabledNav === "backward" ? "disabled" : "moveInner";
+
+  if (!deptData) return null;
+
+  return (
+    <>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        className={classes.titleContainer}
+      >
+        <Grid item>
+          <h1 ref={paginationRef} className="swiperPagination"></h1>
+        </Grid>
+        <SalaryFilter
+          onClickFilter={onClickFilter}
+          value={value}
+          className={classes.title}
+        />
+      </Grid>
+      <Swiper
+        ref={swiperRef}
+        slidesPerView={1}
+        id="main"
+        spaceBetween={40}
+        pagination={{
+          el: ".swiperPagination",
+          type: "custom",
+          renderCustom: (swiper, current, total) => {
+            console.log("index", current);
+            const deptName = deptList[current - 1];
+            return deptName;
+          },
+        }}
+        navigation={{
+          prevEl: navigationPrevRef.current,
+          nextEl: navigationNextRef.current,
+        }}
+        onBeforeInit={(swiper) => {
+          swiper.params.navigation.prevEl = navigationPrevRef.current;
+          swiper.params.navigation.nextEl = navigationNextRef.current;
+          swiper.params.pagination.el = paginationRef.current;
+        }}
+        onSlideChange={() => {
+          disableNav(chartSwiper);
+        }}
+        scrollbar={{ draggable: true }}
+        className={classes.root}
+      >
+        {Object.keys(deptData).map((dept, idx) => {
+          const data = deptData[dept];
+          return (
+            <SwiperSlide key={`chart-${dept}`} className={classes.swiperSlide}>
+              <ResponsiveContainer
+                width="100%"
+                height={380}
+                className={classes.chartContainer}
+              >
+                <BarChart data={data} margin={{ top: 50 }}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="sal" tick={false} />
+                  <YAxis
+                    dataKey="cnt"
+                    type="number"
+                    domain={[0, "dataMax+100"]}
+                  />
+
+                  <Bar
+                    dataKey="cnt"
+                    fill="#8884d8"
+                    label={<CustomizedLabel currentVal={value} />}
+                  >
+                    {data.map((entry, index) => {
+                      const highlight = entry.sal === value;
+                      const color = setChartColor(entry.dept_name, highlight);
+                      return (
+                        <Cell
+                          fill={color}
+                          key={`cell-${index}`}
+                          stroke="false"
+                          strokeDasharray="5,5"
+                        />
+                      );
+                    })}
+                  </Bar>
+                </BarChart>
+              </ResponsiveContainer>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div
+        ref={navigationPrevRef}
+        onClick={() => onClickBackward(chartSwiper)}
+        className={classes.backward}
+      >
+        <ArrowLeftIcon className={classes[backwardClass]} />
+      </div>
+      <div
+        ref={navigationNextRef}
+        onClick={() => onClickForward(chartSwiper)}
+        className={classes.forward}
+      >
+        <ArrowRightIcon className={classes[forwardClass]} />
+      </div>
+    </>
+  );
 };
 
 export default DeptChart;
