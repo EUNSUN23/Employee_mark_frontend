b41e1a9abe2dafe9f3047eae429a76e397ee4f2a
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2021-04-05 17:48:11 +0900

    STAT:CustomizedRadarTick.js)레이블 & 이모티콘 정렬

--- src/components/Home/Graph/CustomizedRadarTick.js
+++ src/components/Home/Graph/CustomizedRadarTick.js
@@ -1,62 +1,68 @@
 import styled from "styled-components";
+import Svg from "../../../shared/svgIcons";
 
-const Tick = styled.span`
-  font-size: ${(props) => (props.value.length > 8 ? "12px" : "14px")};
+const Tick = styled.div`
+  position: relative;
+  width: 170px;
+  left: 0;
+
   margin: 0 20px;
-  padding: ${(props) => (props.value.length <= 16 ? "0px 20px " : "0px 15px ")};
-  color: ${(props) => (props.emp === 0 ? "#999" : "#222")};
-  position: absolute;
-  text-align: center;
+`;
+
+const Label = styled.span`
+  text-align: left;
   font-weight: bold;
-  left: 5%;
-  border: 1px solid black;
-  div {
-    font-weight: normal;
-    color: grey;
-    font-size: 12px;
-  }
+  font-size: ${(props) => (props.value.length <= 16 ? "14px" : "12px")};
+  padding: ${(props) =>
+    props.value.length <= 16 ? "0px 30px 0px" : "0px 30px 0px"};
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Icon = styled.div`
+  width: 23px;
+  height: 23px;
+  left: 0%;
+  position: absolute;
 `;
 
 const CustomizedRadarTick = (tickProps) => {
   const { x, y, payload, data, width, radius, index } = tickProps;
   const { coordinate, value } = payload;
 
-  const d = (Math.round(radius) / 5).toFixed(); // 그래프 크기에 따라서 label간격조정
-
-  console.log("tick", tickProps, "d", d);
+  const d =
+    radius > 180
+      ? (Math.round(radius) / 5).toFixed()
+      : (Math.round(radius) / 3.5).toFixed(); // 그래프 크기에 따라서 label간격조정
 
   const emp = data[index].count;
   const tickVal = typeof value === "string" ? value : `${value / 10000}만`;
 
   const labelAngle = Math.round(coordinate);
 
-  console.log("angle", labelAngle);
+  console.log(value, tickProps);
 
   let XPoint;
   let YPoint;
 
   if (labelAngle < 90 && labelAngle > -70) {
-    XPoint = x - 1 * d;
+    XPoint = radius > 180 ? x + d : x;
     YPoint = y - 10;
-    console.log("first", XPoint, YPoint);
   }
   if (labelAngle <= -70 && labelAngle >= -120) {
-    XPoint = x - 2 * d;
-    YPoint = y + d;
-
-    console.log("2", XPoint, YPoint);
+    XPoint = x - 1.5 * d;
+    YPoint = y + 10;
   }
 
   if (labelAngle <= -150 && labelAngle >= -230) {
-    XPoint = x - 3.3 * d;
+    XPoint = radius > 180 ? x - 3.3 * d : x - 2.5 * d;
     YPoint = y - 10;
-    console.log("3", XPoint, YPoint);
   }
 
   if (labelAngle === 90) {
     XPoint = x - 2 * d;
-    YPoint = y - d;
-    console.log("4", XPoint, YPoint);
+    YPoint = y - 30;
   }
 
   return (
@@ -67,8 +73,11 @@ const CustomizedRadarTick = (tickProps) => {
         emp={emp}
         width={width}
       >
-        {tickVal}
-        <div>{`${emp}명`}</div>
+        <Icon>
+          <Svg label={tickVal} />
+        </Icon>
+        <Label value={tickVal}>{tickVal}</Label>
+        {/* <LabelVal label={tickVal}>{`${emp}명`}</LabelVal> */}
       </Tick>
     </foreignObject>
   );
